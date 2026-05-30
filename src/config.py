64094e1ff68a7fc5a8540e6e53da56ff538441b4
Jainish Shah <jainishnshah@gmail.com>
2026-05-30 23:45:59 +0530

    Update app

--- src/config.py
+++ src/config.py
@@ -5,92 +5,35 @@ sector_dict = {
     "Banking": [
         "HDFCBANK.NS",
         "ICICIBANK.NS",
-        "SBIN.NS",
         "KOTAKBANK.NS",
-        "AXISBANK.NS",
-        "INDUSINDBK.NS",
+        "SBIN.NS",
         "^NSEI"
     ],
-
     "IT": [
         "TCS.NS",
         "INFY.NS",
-        "HCLTECH.NS",
         "WIPRO.NS",
-        "TECHM.NS",
-        "LTIM.NS",
+        "HCLTECH.NS",
         "^NSEI"
     ],
-
     "Energy": [
         "RELIANCE.NS",
         "ONGC.NS",
         "BPCL.NS",
-        "IOC.NS",
-        "POWERGRID.NS",
-        "NTPC.NS",
         "^NSEI"
     ],
-
     "Auto": [
         "MARUTI.NS",
         "M&M.NS",
-        "TATAMOTORS.NS",
-        "BAJAJ-AUTO.NS",
-        "EICHERMOT.NS",
-        "HEROMOTOCO.NS",
         "^NSEI"
     ],
-
     "FMCG": [
         "HINDUNILVR.NS",
         "ITC.NS",
         "NESTLEIND.NS",
-        "BRITANNIA.NS",
-        "TATACONSUM.NS",
-        "DABUR.NS",
-        "^NSEI"
-    ],
-
-    "Pharma": [
-        "SUNPHARMA.NS",
-        "DRREDDY.NS",
-        "CIPLA.NS",
-        "DIVISLAB.NS",
-        "LUPIN.NS",
-        "^NSEI"
-    ],
-
-    "Metals": [
-        "TATASTEEL.NS",
-        "JSWSTEEL.NS",
-        "HINDALCO.NS",
-        "VEDL.NS",
-        "^NSEI"
-    ],
-
-    "Financial Services": [
-        "BAJFINANCE.NS",
-        "BAJAJFINSV.NS",
-        "SBILIFE.NS",
-        "HDFCLIFE.NS",
-        "ICICIPRULI.NS",
-        "^NSEI"
-    ],
-
-    "Telecom": [
-        "BHARTIARTL.NS",
-        "INDUSTOWER.NS",
-        "^NSEI"
-    ],
-
-    "Infrastructure": [
-        "LT.NS",
-        "ULTRACEMCO.NS",
-        "ADANIPORTS.NS",
-        "SIEMENS.NS",
         "^NSEI"
     ]
 }
+
 
 horizon = 5
