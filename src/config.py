ee1f1f97f635739bf160c9c92185921dc282b885
Jainish Shah <jainishnshah@gmail.com>
2026-05-30 23:53:29 +0530

    Update app

--- src/config.py
+++ src/config.py
@@ -2,62 +2,38 @@ START_DATE = "2012-01-01"
 END_DATE = "2025-01-01"
 
 sector_dict = {
-    "Banking_Financials": [
+    "Banking": [
         "HDFCBANK.NS",
         "ICICIBANK.NS",
-        "SBIN.NS",
         "KOTAKBANK.NS",
-        "AXISBANK.NS",
-        "BAJFINANCE.NS",
+        "SBIN.NS",
         "^NSEI"
     ],
     "IT": [
         "TCS.NS",
         "INFY.NS",
-        "HCLTECH.NS",
         "WIPRO.NS",
-        "TECHM.NS",
+        "HCLTECH.NS",
         "^NSEI"
     ],
-    "Energy_Oil_Gas": [
+    "Energy": [
         "RELIANCE.NS",
         "ONGC.NS",
-        "NTPC.NS",
-        "POWERGRID.NS",
         "BPCL.NS",
-        "^NSEI"
-    ],
-    "FMCG": [
-        "ITC.NS",
-        "HINDUNILVR.NS",
-        "NESTLEIND.NS",
-        "BRITANNIA.NS",
-        "TATACONSUM.NS",
         "^NSEI"
     ],
     "Auto": [
         "MARUTI.NS",
         "M&M.NS",
-        "TMPV.NS",        # Tata Motors Passenger Vehicles (Cars & EVs)
-        "TMCV.NS",        # Tata Motors Limited (Commercial Vehicles)
-        "BAJAJ-AUTO.NS",
-        "EICHERMOT.NS",
         "^NSEI"
     ],
-    "Metals_Mining": [
-        "TATASTEEL.NS",
-        "JSWSTEEL.NS",
-        "HINDALCO.NS",
-        "COALINDIA.NS",
-        "^NSEI"
-    ],
-    "Pharma_Healthcare": [
-        "SUNPHARMA.NS",
-        "CIPLA.NS",
-        "DRREDDY.NS",
-        "APOLLOHOSP.NS",
+    "FMCG": [
+        "HINDUNILVR.NS",
+        "ITC.NS",
+        "NESTLEIND.NS",
         "^NSEI"
     ]
 }
+
 
 horizon = 5
